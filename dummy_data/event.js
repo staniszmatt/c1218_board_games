e15d24ccf25fc2893856ee3cf82558d60c7b0819
christophersulayao <christoper.sulayao@gmail.com>
2019-02-28 14:55:57 -0800

    added dummy data for events database, the data has been stringified and pasted to the bottom, but commented out

---
+++ dummy_data/event.js
@@ -0,0 +1,49 @@
+let event = [
+        {
+            id: '1',
+            userId: '1',
+            date: '1984-05-20',
+            time: '06:00:00',
+            location: 'Long Beach, Ca',
+            gameImage: 'www.image.com'
+        },
+        {
+            id: '2',
+            userId: '1',
+            date: '1911-6-30',
+            time: '13:45:00',
+            location: 'Compton, Ca',
+            gameImage: 'www.image.com'
+        },
+        {
+            id: '3',
+            userId: '2',
+            date: '1969-12-25',
+            time: '24:30:00',
+            location: 'Irvine, Ca',
+            gameImage: 'www.image.com'
+        }
+];
+
+// [{
+//     "id": "1",
+//     "userId": "1",
+//     "date": "1984-05-20",
+//     "time": "06:00:00",
+//     "location": "Long Beach, Ca",
+//     "gameImage": "www.image.com"
+// }, {
+//     "id": "2",
+//     "userId": "1",
+//     "date": "1911-6-30",
+//     "time": "13:45:00",
+//     "location": "Compton, Ca",
+//     "gameImage": "www.image.com"
+// }, {
+//     "id": "3",
+//     "userId": "2",
+//     "date": "1969-12-25",
+//     "time": "24:30:00",
+//     "location": "Irvine, Ca",
+//     "gameImage": "www.image.com"
+// }]
