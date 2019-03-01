a7ca3c65e1262fb7bb1d05577f2efcf11e465155
christophersulayao <christoper.sulayao@gmail.com>
2019-03-01 12:50:46 -0800

    created user profile, displays create events, view available events, hosted events, joined events, all are clickable and re-routes to corresponding page

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,16 +4,38 @@ import {Link} from 'react-router-dom';
 export default (props) => {
     return (
         <div>
-            <div> Profile </div>
-            <Link to="/events/new-event" className="nav-link">Create Event</Link>
-            <br/>
-            <Link to="/events" className="nav-link">View Available Events</Link>
-            <br/>
-            <Link to="/events/host" className="nav-link">My Hosted Events</Link>
-            <br/>
-            <Link to="/events/myevents" className="nav-link">My Joined Events</Link>
-            
+            <h1 className="center"> Profile Page </h1>
+            <div className="center"> User Profile Picture here</div>
+            <div className="center"> Scott Bowler</div>
+            <div className="center">
+                <div>
+                    <Link to="/events/new-event" className="nav-link red black-text">Create Event</Link>
+                </div>
+
+                <div>
+                    <Link to="/events" className="nav-link blue black-text">View Available Events</Link>
+                </div>
+
+                <div>
+                    <Link to="/events/host" className="nav-link red black-text">My Hosted Events</Link>
+                </div>
+
+                <div>
+                    <Link to="/events/myevents" className="nav-link blue black-text">My Joined Events</Link>
+                </div>
+            </div>
+
         </div>
 
     );
 }
+
+
+
+
+
+
+
+
+
+
