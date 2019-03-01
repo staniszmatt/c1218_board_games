be7c73e67e3ac8f8502850752b9be7974baa70b1
christophersulayao <christoper.sulayao@gmail.com>
2019-03-01 12:07:28 -0800

    finished  event information page

--- src/components/events.js
+++ src/components/events.js
@@ -3,35 +3,8 @@ import { Link } from 'react-router-dom';
 
 export default (props) => {
     return (
-       <div>
-           <h1 className="center "> Event Information</h1>
+       <div className="center ">
 
-          <div>
-              <div>Host: Topher Uchiha</div>
-
-              <div>Game Title: Magic The Gathering</div>
-
-              <div>
-                  <ul>
-                      <li>Date: 4-20-1984</li>
-                      <li>Time: 1:00P.M.</li>
-                  </ul>
-              </div>
-
-              <div>
-                  <ul>
-                      <li>Address: Some Address</li>
-                      <li>City: Beverly Hills</li>
-                      <li>State: CA</li>
-                      <li>Zip: 90210</li>
-                  </ul>
-              </div>
-
-              <div>
-                  <div> 4 - 6 </div>
-              </div>
-
-          </div>
            <div>
                <Link to="/events/id" className="nav-link">Game 1</Link>
                <h1>game2</h1>
