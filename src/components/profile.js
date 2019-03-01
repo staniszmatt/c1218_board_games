9fdd152b04c5801daa02633451e5070922b75677
beeeweee <billdarnalljr@gmail.com>
2019-03-01 10:02:34 -0800

    events page

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,8 +6,11 @@ export default (props) => {
         <div>
             <div> Profile </div>
             <Link to="/events/new-event" className="nav-link">Create Event</Link>
+            <br/>
             <Link to="/events" className="nav-link">View Available Events</Link>
+            <br/>
             <Link to="/events/host" className="nav-link">My Hosted Events</Link>
+            <br/>
             <Link to="/events/myevents" className="nav-link">My Joined Events</Link>
             
         </div>
