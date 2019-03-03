75c218b5ce8db367b0cff97256feab370ec18393
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-03 10:53:28 -0800

    fixed missing address for player profile. Formatted to grop address into an object inside locations of profile.

--- api/profile.php
+++ api/profile.php
@@ -7,12 +7,27 @@ $output = [
 ];
 
 $id = $_GET['id'];
-$query = "SELECT * FROM `profile` WHERE `id` = $id";
+$query = "SELECT p.*, 
+            l.streetAddress, l.city, l.state, l.zipcode 
+         FROM profile AS p
+         JOIN location AS l
+            ON p.location = l.id
+         WHERE p.id = $id";
 $result = $db->query($query);
-
 $data = [];
 
 while($row = $result->fetch_assoc()){
+   $output['success'] = true;
+   $row['location'] = [
+      'streetAddress'=> $row['streetAddress'],
+      'city'=> $row['city'],
+      'state'=> $row['state'],
+      'zipcode'=> $row['zipcode']
+   ];
+   unset($row['streetAddress'], 
+   $row['city'],
+   $row['state'],
+   $row['zipcode']);
    $data[] = $row;
 }
 
