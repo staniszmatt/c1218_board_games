91efee6135db1be806a8f7e207cad3ea0814b277
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-04 14:57:40 -0800

    Created end point for single event to join created eents-eventID.php file grabs a specific event to decide if you want to join or not.

---
+++ api/events-eventID.php
@@ -0,0 +1,61 @@
+<?php
+require_once('../config/setup.php');
+require_once('../config/mysql_connect.php');
+
+$output = [
+   'success'=> false
+];
+
+$eventID = $_GET['eventID'];
+$query = "SELECT e.*, 
+            l.streetAddress, l.city, l.state, l.zipcode, p.playerName,
+            pl.userID AS player, pl.id AS playerID
+         FROM event AS e
+         JOIN location AS l
+            ON e.location = l.id
+         RIGHT JOIN playerList AS pl
+            ON pl.eventID = e.id
+         JOIN profile AS p
+            ON pl.userID = p.id
+         WHERE e.id = $eventID";
+$result = $db->query($query);
+$data = [];
+
+if ($result){
+   $output['success'] = true;
+
+   while($row = $result->fetch_assoc()){
+      if(empty($data[$row['id']])){
+         $row['location'] = [
+            'streetAddress'=> $row['streetAddress'],
+            'city'=> $row['city'],
+            'state'=> $row['state'],
+            'zipcode'=> $row['zipcode']
+         ];  
+         $row['playerList'][] = [
+            'playerName'=> $row['playerName'],
+            'player'=> $row['player'],
+            'playerID'=> $row['playerID']
+         ];
+         unset($row['streetAddress'], 
+               $row['city'],
+               $row['state'],
+               $row['zipcode'],
+               $row['player'],
+               $row['playerID'],
+               $row['playerName']);           
+         $data[$row['id']] = $row;
+      } else {
+         $data[$row['id']]['playerList'][] = [
+            'playerName'=> $row['playerName'],
+            'player'=> $row['player'],
+            'playerID'=> $row['playerID']
+         ];
+      }
+   }
+}
+
+$output['data'] = $data;
+$json_output = json_encode($output);
+print($json_output);
+?>
