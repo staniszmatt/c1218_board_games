6d93a5deca8c622dd1608f9bccb12a4cd32bf25c
christophersulayao <christoper.sulayao@gmail.com>
2019-03-06 12:12:30 -0800

    added axios funtionality, verified that data was being recieved, and that data was being displayed on the dom

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import UserData from '../../../dummy_data/profile';
 import './profile.css';
+import axios from 'axios';
 
 class Profile extends Component {
     state = {
@@ -11,7 +12,11 @@ class Profile extends Component {
         this.getUserProfile();
     }
 
-    getUserProfile(){
+    async getUserProfile(){
+        const resp = await axios.get('/api/profile.php?id=1');
+
+        console.log('resp: ', resp);
+
         const userId = 0;
         this.setState({
             userProfile: UserData[userId] || []
@@ -29,19 +34,19 @@ class Profile extends Component {
                 </div>
                 <div className="profile-button-container center">
                     <div>
-                        <Link to="/new-event" className="btn nav-link red black-text">Create Event</Link>
+                        <Link to="/new-event" className="btn nav-link ">Create Event</Link>
                     </div>
 
                     <div>
-                        <Link to="/events" className="btn nav-link blue black-text">View Available Events</Link>
+                        <Link to="/events" className="btn nav-link ">View Available Events</Link>
                     </div>
 
                     <div>
-                        <Link to="/events/host" className="btn nav-link red black-text">My Hosted Events</Link>
+                        <Link to="/events/host" className="btn nav-link ">My Hosted Events</Link>
                     </div>
 
                     <div>
-                        <Link to="/events/myevents" className="btn nav-link blue black-text">My Joined Events</Link>
+                        <Link to="/events/myevents" className="btn nav-link ">My Joined Events</Link>
                     </div>
                 </div>
             </div>
