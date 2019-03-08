292c2123f4dfdd095961be0cdaf1952c559e202c
christophersulayao <christoper.sulayao@gmail.com>
2019-03-08 10:11:08 -0800

    need to fix url to display current id

--- src/components/event-id/event-id.js
+++ src/components/event-id/event-id.js
@@ -7,33 +7,30 @@ import axios from 'axios';
 
 class EventSelected extends Component {
     state = {
-        event: null
+        eventId: null,
     }
+
 
     async componentDidMount() {
         const ID = this.props.match.params.id;
-
         const resp = await axios.get(`/api/events-eventID.php?eventID=${ID}`);
-
         this.setState({
-            event: resp.data.event
+            eventId: resp.data.event
         });
-    }
 
+    }
     sendUserData = async ()=> {
         const resp = await axios.post(`/api/events-id-join.php`,{
             userID: 5,
             eventID: this.props.match.params.id
         });
-
-        console.log("Sent Player Response ", resp);
-
-        this.props.history.push("/events/" + this.state.event.eventID + "/player-list")
+        console.log("Sent Player Response ", resp)
+        this.props.history.push("/events/" + this.state.eventId + "/player-list")
     }
     render() {
-        const { event } = this.state;
+        const { eventId } = this.state;
 
-        if (event === null) {
+        if (eventId === null) {
             return (
                 <div className="center">
                     <div className="main-container">
@@ -44,40 +41,40 @@ class EventSelected extends Component {
                 </div>
             );
         } else {
-            const numberOfPlayers = event.playerList.length;
+            const numberOfPlayers = eventId.playerList.length;
             return (
                 <div className="center">
                     <div className="main-container">
                         <div className="btn game-picture center">
-                            <img src={event.gameImages}></img>
+                            <img src={eventId.gameImages}></img>
                         </div>
-                        <div className="btn event-host grey ">Game: {event.gameTitle}</div>
-                        <div className="btn event-host grey darken-1">Host Name: {event.playerList[0].playerName}</div>
-    
-    
+                        <div className="btn event-host grey ">Game: {eventId.gameTitle}</div>
+                        <div className="btn event-host grey darken-1">Host Name: {eventId.playerList[0].playerName}</div>
+
+
                         <div className="date grey">
                             <ul>
-                                <li>Date: {event.date}</li>
-                                <li>Start: {event.startTime}</li>
+                                <li>Date: {eventId.date}</li>
+                                <li>Start: {eventId.startTime}</li>
                             </ul>
                         </div>
-    
+
                         <div className="btn address grey darken-1">
                             <ul>
-                                <li>Address: {event.location.streetAddress}</li>
-                                <li>City: {event.location.city}</li>
-                                <li>State: {event.location.state}</li>
-                                <li>Zip: {event.location.zipCode}</li>
+                                <li>Address: {eventId.location.streetAddress}</li>
+                                <li>City: {eventId.location.city}</li>
+                                <li>State: {eventId.location.state}</li>
+                                <li>Zip: {eventId.location.zipCode}</li>
                             </ul>
                         </div>
-    
+
                         <div className=" numberOfPlayers grey">
-                            <div> {numberOfPlayers} Players out of {event.playerLimit} have joined </div>
+                            <div> {numberOfPlayers} Players out of {eventId.playerLimit} have joined </div>
                         </div>
-    
+
                         <div className="center joinButton green lighten-4">
                         <button className="btn" onClick={this.sendUserData}></button>
-                            {/* <Link onClick={this.sendUserData} to={"/events/" + event.eventID + "/player-list"} className="nav-link">Join Game</Link> */}
+                            {/* <Link onClick={this.sendUserData} to={"/events/" + eventId.eventID + "/player-list"} className="nav-link">Join Game</Link> */}
                         </div>
                     </div>
                 </div>
