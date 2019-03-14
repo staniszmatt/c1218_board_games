a53db19492b39c1b09f700de4c593d53be6cd7e1
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-13 18:46:05 -0700

    updated events-new-event end point fixed a line of code that was screwing up the eventID being valid

--- public/api/events-eventID-edit.php
+++ public/api/events-eventID-edit.php
@@ -2,57 +2,61 @@
 session_start();
 require_once('../../config/setup.php');
 require_once('../../config/mysql_connect.php');
-//TODO: Setup an array of data fields that is allowed to change and reject the rest 
-$dataCheck = [ "eventID", "date", "startTime", "endTime", "gameTitle", "gameImages"];
+
+$dataCheck = [ "eventID", "date", "startTime", "endTime", "gameTitle", "gameImages", "playerLimit", 
+               "streetAddress", "city", "state", "zipcode" ];
 
 if (!isset($_SESSION['userID'])){
    $output['success'] = true;
    $output['logged-in'] = false;
 } else {
-   print(!isset($_SESSION));
    $data = json_decode( file_get_contents( 'php://input'),true);
-
    if(!$data){
       $output['success']=false;
       exit();
    }
-   
    $output = [
       "success"=> false,
       "output"=> []
    ];
-   
    foreach($data as $key=>$value){
       $data[$key] = addslashes($value);
-   }
-   
-   if(!empty($output['error'])){
-      print(json_encode($output));
-      exit();
-   }
+   } 
+   //Get Event ID and remove it from the assoc array
    $eventID = $data['eventID'];
    unset($data['eventID']);
+   //Get Location ID to submit changes
+   $query = "SELECT location FROM event WHERE event.id = {$eventID}";
+   $result = $db->query($query);
 
-   foreach($data as $key=>$value){
+   if(!$result){
+      $output['success']=false;
+      $output['error']="Location not found for event!";
+      exit();
+   }
+   while($row = $result->fetch_assoc()){
+      $locationID = $row['location'];
+   }
+  foreach($data as $key=>$value){
       $checkValue = false;
+      //Verify Key names are allowed to be edited
       for ($index = 0; $index < sizeof($dataCheck); $index++ ){
          if ($key === $dataCheck[$index]){
             $checkValue = true; 
          }
       }
-      echo($checkValue);
       if (!$checkValue){
          $output['error'][] = "Can not edit {$key}!";
       } 
       else {
-         $query = "UPDATE event SET {$key} = '{$value}' WHERE id = '{$eventID}'";
-         echo("<div>");
-         print_r($query."<br>");
-         echo("</div>");
+         if ($key === "streetAddress" || $key === "city" || $key === "state" || $key === "zipcode"){
+            $query = "UPDATE location SET {$key} = '{$value}' WHERE id = {$locationID}";
+         } else {
+            $query = "UPDATE event SET {$key} = '{$value}' WHERE id = '{$eventID}'";
+         }
+         print_r($query);
+         print_r($result);
          $result = $db->query($query);
-         echo("<div>");
-         echo($result."<br>");
-         echo("</div>");
          if (!$result){
             $output['error'][] = $result;
          } else {
