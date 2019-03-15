fce5524f475a847d3692f1b94cd36ef98dd8e575
staniszmatt <matthewpstaniszewski@gmail.com>
2019-03-14 17:53:05 -0700

    updated error handling on sign-up page removed original date check added error_check_fuctions to use as list of error handling functions

--- config/check_date.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function checkmydate($date) {
-   $tempDate = explode('-', $date);
-   // checkdate(month, day, year)
-   return checkdate($tempDate[1], $tempDate[2], $tempDate[0]);
- }
-
-?>
--- public/api/sign-up.php
+++ public/api/sign-up.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once('../../config/check_date.php');
+require_once('../../config/error_check_functions.php');
 require_once('../../config/setup.php');
 require_once('../../config/mysql_connect.php');
 
@@ -25,17 +25,32 @@ if (!isset($_SESSION['userID'])){
       print(json_encode($output));
       exit();
    }
-
-   if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+   if((!checkStrg20($data['playerName'])) || (!checkStrg20($data['firstName'])) || !checkStrg20($data['city']) || !checkStrg20($data['state'])){
+      throw new Exception("Player name, first name, city or state is to long!");
+   }
+   if((!checkStrg30($data['email'])) || (!checkStrg30($data['lastName'])) || !checkStrg30($data['password']) || !checkStrg30($data['streetAddress'])){
+      throw new Exception("Last name, email or password is to long!");
+   }
+   if((!checkEmail($data['email']))){
       throw new Exception("Enter a valid email address!");
-   };
+   }
+   if ((!checkMyDate($data['dateOfBirth']))){
+      throw new Exception("Please enter the correct format for date of birth.");
+   }
+   if ((!checkPhoneNum($data['phone']))){
+      throw new Exception("Please enter a proper phone number");
+   }
+   if ((!checkZip($data['zipcode']))){
+      throw new Exception("Please enter a proper zip code");
+   }
+
    $password = $data["password"];
    unset($data["password"]);
    $location = null;  
    for($i = 0; $i < 2000; $i++){
       $password = hash("sha256", $password, false);
    }
-
+   
    $query = "INSERT INTO location 
                SET streetAddress='{$data['streetAddress']}', 
                   city='{$data['city']}',
@@ -50,9 +65,9 @@ if (!isset($_SESSION['userID'])){
       $output['error'] = mysqli_error($db);
    }
    $jointDate = date("y-m-d h:i:s");
-   if(checkmydate($data['dateOfBirth']) !== 1){
-      throw new Exception("Please enter correct date format.");
-   }
+   
+
+
    $query = "INSERT INTO profile 
             SET playerName='{$data['playerName']}',
                firstName='{$data['firstName']}',
