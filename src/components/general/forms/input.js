9e4e4e29cb35f5c66e20e4f1928f8004a34a0a10
staniszmatt <matthewpstaniszewski@gmail.com>
2019-04-25 11:42:59 -0700

    Updated sign-in page and forms styling

--- src/components/general/forms/input.js
+++ src/components/general/forms/input.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../../../assets/css/forms.css'
 
 export default props => {
 
     const { input, label, type='text', meta: {error, touched}} = props;
     return(
-        <div>
-            <input {...input} type={type ? type : 'text'} autoComplete="off"  />
-            <label id="input-label"> {label} </label>
+        <div className="form-container" >
+            <input className="form-field" {...input} type={type ? type : 'text'} autoComplete="off"  />
+            <label className="form-label" id="input-label"> {label} </label>
             <p className="red-text darken-2" > {touched && error} </p>
         </div>
     );
