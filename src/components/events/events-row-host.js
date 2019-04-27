1401c9493cac50532e4b2271b11e2ab679f94d77
staniszmatt <matthewpstaniszewski@gmail.com>
2019-04-27 14:49:11 -0700

    Fixed Time display on all events lists to 12 hour

--- src/components/events/events-row-host.js
+++ src/components/events/events-row-host.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import timeTo12Hours from '../../helper/timeTo12Hours';
 
 const EventRowHost = (props) => {
 console.log("events-row-host ", props);
     const { startTime, endTime, date, gameTitle, eventID, playerLimit, playerCount } = props.event;
 
-    if (parseInt(playerCount) < parseInt(playerLimit)) {
-        return (
+    if (parseInt(playerCount) < parseInt(playerLimit) && props.event) {
 
+        if (props.event){
+            const startTime12H = timeTo12Hours(startTime);
+            const endTime12H = timeTo12Hours(endTime);
+
+            return (
+                <div className="events-container col s12 l6 nav-link center" key={eventID}>
+                    <Link to={'/events/' + eventID + '/host'} className="events-list-button ">
+                        <p className="events-title">{gameTitle}</p>
+                        <p className="events-player-count">Players: {playerCount} out of {playerLimit}</p>
+                        <p className="events-date-time">{date} {startTime12H} {endTime12H}</p>
+                    </Link>
+                </div>
+            );
+        }
+    }
+    
+    if (props.event){
+        const startTime12H = timeTo12Hours(startTime);
+        const endTime12H = timeTo12Hours(endTime);
+        return (
             <div className="events-container col s12 l6 nav-link center" key={eventID}>
-                <Link to={'/events/' + eventID + '/host'} className="events-list-button ">
+                <Link to={'/events/' + eventID + '/host'} className="events-list-button">
                     <p className="events-title">{gameTitle}</p>
-                    <p className="events-player-count">Players: {playerCount} out of {playerLimit}</p>
-                    <p className="events-date-time">{date} {startTime} {endTime}</p>
+                    <p className="events-player-count">Game Is Full</p>
+                    <p className="events-date-time">{date} {startTime12H} {endTime12H}</p>
                 </Link>
             </div>
         );
     }
-    return (
-        <div className="events-container col s12 l6 nav-link center" key={eventID}>
-            <Link to={'/events/' + eventID + '/host'} className="events-list-button">
-                <p className="events-title">{gameTitle}</p>
-                <p className="events-player-count">Game Is Full</p>
-                <p className="events-date-time">{date} {startTime} {endTime}</p>
-            </Link>
-        </div>
-    )
 }
 
 export default EventRowHost;
--- src/components/events/events-row-myevents.js
+++ src/components/events/events-row-myevents.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import BoardGamePic from '../../assets/images/boardgame_default.jpg';
+import timeTo12Hours from '../../helper/timeTo12Hours';
 
 const EventRowMyEvents = (props) => {
 
     const { startTime, endTime, date, gameTitle, eventID, playerLimit, playerCount } = props.event;
 console.log("events-row-myevents.js ", props);
     if (parseInt(playerCount) < parseInt(playerLimit)) {
+        if (props.event){
+            const startTime12H = timeTo12Hours(startTime);
+            const endTime12H = timeTo12Hours(endTime);
+        
+            return (
+                <div className="events-container col s12 l6 nav-link center" key={eventID}>
+                    <Link to={'/events/' + eventID + '/myevents'} className="events-list-button">
+                        <p className="events-title">{gameTitle}</p>
+                        <p className="events-player-count">Players: {playerCount} out of {playerLimit}</p>
+                        <p className="events-date-time">{date} {startTime12H} {endTime12H}</p>
+                    </Link>
+                </div>
+            );
+        }
+
+    }
+    if (props.event){
+        const startTime12H = timeTo12Hours(startTime);
+        const endTime12H = timeTo12Hours(endTime);
+    
         return (
             <div className="events-container col s12 l6 nav-link center" key={eventID}>
                 <Link to={'/events/' + eventID + '/myevents'} className="events-list-button">
                     <p className="events-title">{gameTitle}</p>
-                    <p className="events-player-count">Players: {playerCount} out of {playerLimit}</p>
-                    <p className="events-date-time">{date} {startTime} {endTime}</p>
+                    <p className="events-player-count">Game Is Full</p>
+                    <p className="events-date-time">{date} {startTime12H} {endTime12H}</p>
                 </Link>
             </div>
         );
     }
-    return (
-        <div className="events-container col s12 l6 nav-link center" key={eventID}>
-            <Link to={'/events/' + eventID + '/myevents'} className="events-list-button">
-                <p className="events-title">{gameTitle}</p>
-                <p className="events-player-count">Game Is Full</p>
-                <p className="events-date-time">{date} {startTime} {endTime}</p>
-            </Link>
-        </div>
-    )
 }
 
 export default EventRowMyEvents;
